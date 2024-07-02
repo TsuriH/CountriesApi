e71a84ced0314f58df768f54223b7264b20bf3f4
Tsuri <tsuri.he@gmail.com>
2024-07-02 12:59:10 +0300

    dropdown menu

--- src/Components/Layout/Main/Main.tsx
+++ src/Components/Layout/Main/Main.tsx
@@ -1,9 +1,48 @@
+import { faChevronDown, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Main.css";
+import { useState } from "react";
 
 export function Main(): JSX.Element {
+
+    const [dropdownHeader, setDropdownHeader] = useState("Filter by Region")
+    const [optionsAreOpen, setOptionsAreOpen] = useState(false)
+
     return (
         <div className="Main">
-			
+            <div className="main-header">
+                <div className="search-bar">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} style={{
+                        color: "white"
+                    }} />
+                    <input type="text" placeholder="Search for a country..." />
+                </div>
+
+                <div className="dropdown">
+
+                    <div className="dropdown-header" onClick={() => setOptionsAreOpen(!optionsAreOpen)}>
+                        <button >{dropdownHeader} </button>
+                        <FontAwesomeIcon icon={faChevronDown} className="drop-icon" />
+                    </div>
+
+                    <div className="options-wrapper" style={{ height: optionsAreOpen ? '200px' : '0px'}}  >
+                        <div className="dropdown-list">
+                            <button >Africa</button>
+                            <button>America</button>
+                            <button>Asia</button>
+                            <button>Europe</button>
+                            <button>Oceania</button>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="countries-container">
+
+            </div>
+
         </div>
     );
 }
